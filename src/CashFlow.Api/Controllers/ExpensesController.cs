fe28b6f28e96034906c2d37fbb6cb026fea3c177
tainan <tainanzart713@gmail.com>
2024-05-29 11:36:37 -0300

    Tratamento de Errors

--- src/CashFlow.Api/Controllers/ExpensesController.cs
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -12,10 +12,23 @@ public class ExpensesController : ControllerBase
 
     public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
     {
-        var useCase = new RegisterExpenseUseCase();
+        try
+        {
 
-        var response = useCase.Execute(request);
+            var useCase = new RegisterExpenseUseCase();
 
-        return Created( string.Empty , response);
+            var response = useCase.Execute(request);
+
+            return Created(string.Empty, response);
+
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+        catch
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, "unknown error");
+        }
     }
 }
--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
@@ -6,7 +7,35 @@ public class RegisterExpenseUseCase
 {
     public ResponseRegisterExpenseJson Execute(RequestRegisterExpenseJson request)
     {
-        
+        Validate(request);
         return new ResponseRegisterExpenseJson();
+    }
+
+
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+        var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
+        if(titleIsEmpty)
+        {
+            throw new ArgumentException("The title is required.");
+        }
+        
+        if(request.Amount <= 0)
+        {
+            throw new ArgumentException("The Amount must be greater thean zero");
+        }
+
+        var result = DateTime.Compare(request.Date, DateTime.UtcNow);
+        if(result > 0) 
+        {
+            throw new ArgumentException("Expenses cannot be for the future");
+        }
+
+        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.paymentType);
+
+        if (paymentTypeIsValid == false)
+        {
+            throw new ArgumentException("Payment Type is not valid.");
+        }
     }
 }
